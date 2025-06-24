b51aa5ad234e58f34f02918d401f3b52d4440044
Shino Mythmaker <shinomythmaker@gmail.com>
2025-06-24 23:05:47 +0200

    Adds character import and rig editor
    Introduces operators for importing character models with various options like merging meshes and importing Meddle shaders.
    
    Also adds a rig editor panel for generating spring tails and controlling spring bone physics.

---
+++ aetherblend/ui/import_panel.py
@@ -0,0 +1,31 @@
+import bpy
+from ..status import AetherBlendStatus as status
+
+class AETHER_PT_ImportPanel(bpy.types.Panel):
+    bl_label = "Import"
+    bl_idname = "AETHER_PT_import_panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'  
+    bl_category = 'AetherBlend'
+    bl_order = 1 
+
+    def draw(self, context):
+        if status.get_prompt_user():
+            return
+    
+        layout = self.layout
+
+        if status.meddle_installed and status.meddle_enabled:
+            row = layout.row(align=True)
+            row.operator("aether.character_import", text="Import Character", icon = "IMPORT")
+
+def register():
+    bpy.utils.register_class(AETHER_PT_ImportPanel)
+
+def unregister():
+    bpy.utils.unregister_class(AETHER_PT_ImportPanel) 
+ 
+
+
+            
+            
