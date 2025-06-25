904a0a403e0c20b4d9c763da00f9da5c969e4476
Shino Mythmaker <shinomythmaker@gmail.com>
2025-06-26 01:40:43 +0200

    updated branch name

--- aetherblend/preferences.py
+++ aetherblend/preferences.py
@@ -21,7 +21,7 @@ class AetherBlendPreferences(bpy.types.AddonPreferences):
             ('testing', "Testing", "Experimental features"),
             ('dev', "Developement", "Active dev branch")
         ],
-        default='dev',
+        default='main',
         update=branch_changed 
     ) # type: ignore
 
