499cd0d88b424f09200fcc43595b947ceefecadb
Shino Mythmaker <shinomythmaker@gmail.com>
2026-01-16 03:51:01 +0100

    reorganized lists

--- data/bone_mappings/__init__.py
+++ data/bone_mappings/__init__.py
@@ -2,9 +2,5 @@ from .humanoid import *
 from .. generate import BoneGroup
 
 HUMAN: list[list[BoneGroup]] = [
-    [BoneGroup(
-        name="Right Arm",
-        bones=ARM_R,
-        description="Right arm bones including upper arm, forearm, hand, and tweak bones"
-    )],
+    [ARM_R],
 ]
--- data/bone_mappings/humanoid.py
+++ data/bone_mappings/humanoid.py
@@ -1,89 +1,94 @@
-from .. generate import BoneGenerator, ConnectBone, ExtensionBone, PoseOperations, BoneGenerator
+from .. generate import ConnectBone, ExtensionBone, PoseOperations, BoneGroup
 from .. import rigify
 
 # Individual bone list (for backwards compatibility and direct access)
-ARM_R: list[BoneGenerator] = [
-    # Right Arm
-    ConnectBone(
-        name="upper_arm.R", 
-        bone_a="j_ude_a_r", 
-        bone_b="j_ude_b_r", 
-        parent="clavicle.R",
-        req_bones=["j_ude_a_r", "j_ude_b_r"],
-        pose_operations=PoseOperations(
-            rigify_settings=rigify.types.limbs_arm(bone_name="upper_arm.R", fk_coll="Arm.R (FK)", tweak_coll="Arm.R (Tweak)"),
-            b_collection="Arm.R (IK)"
-        )
-    ),
-    ConnectBone(
-        name="forearm.R", 
-        bone_a="j_ude_b_r", 
-        bone_b="j_te_r", 
-        parent="upper_arm.R", 
-        is_connected=True,
-        req_bones=["j_ude_b_r", "j_te_r"],
-        pose_operations=PoseOperations(
-            b_collection="Arm.R (IK)"
-        )
-    ),
-    ExtensionBone(
-        name="hand.R", 
-        bone_a="forearm.R", 
-        size_factor=0.6, 
-        axis_type="local", 
-        axis="Y", 
-        is_connected=True, 
-        parent="forearm.R",
-        req_bones=["forearm.R"],
-        pose_operations=PoseOperations(
-            b_collection="Arm.R (IK)"
-        )
-    ),
-    ExtensionBone(
-        name="wrist.R", 
-        bone_a="n_hte_r", 
-        size_factor=1, 
-        axis_type="local", 
-        axis="Y", 
-        start="head", 
-        parent="hand.R", 
-        is_connected=False, 
-        req_bones=["n_hte_r"],
-        pose_operations=PoseOperations(
-            rigify_settings=rigify.types.basic_super_copy(bone_name="wrist.R", widget_type="sphere"),
-            b_collection="Arm.R (Tweak)"
-        )
-    ),
-    ExtensionBone(
-        name="elbow.R", 
-        bone_a="n_hhiji_r", 
-        axis_type="local", 
-        axis="Y", 
-        start="head", 
-        parent="forearm.R", 
-        is_connected=False, 
-        size_factor=0.5, 
-        roll=45,
-        req_bones=["n_hhiji_r"],
-        pose_operations=PoseOperations(
-            rigify_settings=rigify.types.basic_super_copy(bone_name="elbow.R", widget_type="sphere"),
-            b_collection="Arm.R (Tweak)"
-        )
-    ),
-    ExtensionBone(
-        name="shoulder_tweak.R", 
-        bone_a="n_hkata_r", 
-        axis_type="local", 
-        axis="Y", 
-        start="head", 
-        parent="upper_arm.R", 
-        is_connected=False, 
-        size_factor=0.5, 
-        roll=45,
-        req_bones=["n_hkata_r"],
-        pose_operations=PoseOperations(
-            rigify_settings=rigify.types.basic_super_copy(bone_name="shoulder_tweak.R", widget_type="sphere"),
-            b_collection="Arm.R (Tweak)"
-        )
-    ),
-]
+ARM_R = BoneGroup(
+        name="Right Arm",
+        bones = [
+            # Right Arm
+            ConnectBone(
+                name="upper_arm.R", 
+                bone_a="j_ude_a_r", 
+                bone_b="j_ude_b_r", 
+                parent="clavicle.R",
+                req_bones=["j_ude_a_r", "j_ude_b_r"],
+                pose_operations=PoseOperations(
+                    rigify_settings=rigify.types.limbs_arm(bone_name="upper_arm.R", fk_coll="Arm.R (FK)", tweak_coll="Arm.R (Tweak)"),
+                    b_collection="Arm.R (IK)"
+                )
+            ),
+            ConnectBone(
+                name="forearm.R", 
+                bone_a="j_ude_b_r", 
+                bone_b="j_te_r", 
+                parent="upper_arm.R", 
+                is_connected=True,
+                req_bones=["j_ude_b_r", "j_te_r"],
+                pose_operations=PoseOperations(
+                    b_collection="Arm.R (IK)"
+                )
+            ),
+            ExtensionBone(
+                name="hand.R", 
+                bone_a="forearm.R", 
+                size_factor=0.6, 
+                axis_type="local", 
+                axis="Y", 
+                is_connected=True, 
+                parent="forearm.R",
+                req_bones=["forearm.R"],
+                pose_operations=PoseOperations(
+                    b_collection="Arm.R (IK)"
+                )
+            ),
+            ExtensionBone(
+                name="wrist.R", 
+                bone_a="n_hte_r", 
+                size_factor=1, 
+                axis_type="local", 
+                axis="Y", 
+                start="head", 
+                parent="hand.R", 
+                is_connected=False, 
+                req_bones=["n_hte_r"],
+                pose_operations=PoseOperations(
+                    rigify_settings=rigify.types.basic_super_copy(bone_name="wrist.R", widget_type="sphere"),
+                    b_collection="Arm.R (Tweak)"
+                )
+            ),
+            ExtensionBone(
+                name="elbow.R", 
+                bone_a="n_hhiji_r", 
+                axis_type="local", 
+                axis="Y", 
+                start="head", 
+                parent="forearm.R", 
+                is_connected=False, 
+                size_factor=0.5, 
+                roll=45,
+                req_bones=["n_hhiji_r"],
+                pose_operations=PoseOperations(
+                    rigify_settings=rigify.types.basic_super_copy(bone_name="elbow.R", widget_type="sphere"),
+                    b_collection="Arm.R (Tweak)"
+                )
+            ),
+            ExtensionBone(
+                name="shoulder_tweak.R", 
+                bone_a="n_hkata_r", 
+                axis_type="local", 
+                axis="Y", 
+                start="head", 
+                parent="upper_arm.R", 
+                is_connected=False, 
+                size_factor=0.5, 
+                roll=45,
+                req_bones=["n_hkata_r"],
+                pose_operations=PoseOperations(
+                    rigify_settings=rigify.types.basic_super_copy(bone_name="shoulder_tweak.R", widget_type="sphere"),
+                    b_collection="Arm.R (Tweak)"
+                )
+            ),
+        ],
+        description="Right arm bones including upper arm, forearm, hand, and tweak bones"
+    )
+
