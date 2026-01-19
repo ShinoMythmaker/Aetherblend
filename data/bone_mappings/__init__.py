334308bc4c1edc846bbecd729ddc7ec2a6e291ee
Oats <ayamichi.rei@gmail.com>
2026-01-19 14:06:07 +0200

    Bone Mappings

--- data/bone_mappings/__init__.py
+++ data/bone_mappings/__init__.py
@@ -2,5 +2,10 @@ from .humanoid import *
 from .. generate import BoneGroup
 
 HUMAN: list[list[BoneGroup]] = [
+    [SPINE],
     [ARM_R],
+    [ARM_L],
+    [LEG_R],
+    [LEG_L],
+    [SKIRT_R],
 ]
