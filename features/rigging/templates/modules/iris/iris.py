3a1fa737411679c1e57fb105765876b4e14e93ca
Oats <ayamichi.rei@gmail.com>
2026-05-29 15:38:05 +0200

    Eye Scaling
    It works chief

--- features/rigging/templates/modules/iris/iris.py
+++ features/rigging/templates/modules/iris/iris.py
@@ -24,6 +24,47 @@ IRIS_CONTROLS = BoneGroup(
             operations=[
                RigifyTypeOperation(bone_name="Iris.L", rigify_type=rigify.types.basic_super_copy(widget_type="circle")),
                CollectionOperation(bone_name="Iris.L", collection_name="Face (Primary)"),
+               DriverOperation(
+                   driver_name="Iris.L Scale X",
+                   data="iris",
+                   property='["Eye Scale X"]',
+                   driver=Driver(
+                       type="SCRIPTED",
+                       expression="1/iris_x",
+                       variables=[
+                           TransformChannelVariable(name="iris_x", target_bone="j_f_irisprm_l", transform_type="SCALE_X", transform_space="LOCAL_SPACE"),
+                       ]
+                   ),
+                   time="Post",
+               ),
+               DriverOperation(
+                   driver_name="Iris.L Scale Y",
+                   data="iris",
+                   property='["Eye Scale Y"]',
+                   driver=Driver(
+                       type="SCRIPTED",
+                       expression="1/iris_y",
+                       variables=[
+                           TransformChannelVariable(name="iris_y", target_bone="j_f_irisprm_l", transform_type="SCALE_Z", transform_space="LOCAL_SPACE"),
+                           #Don't mind that it's taking the Z scale, it works
+                       ]
+                   ),
+                   time="Post",
+               ),
+               DriverOperation(
+                   driver_name="Iris.L Scale Z",
+                   data="iris",
+                   property='["Pupil Scale"]',
+                   driver=Driver(
+                       type="SCRIPTED",
+                       expression="iris_z-1",
+                       variables=[
+                           TransformChannelVariable(name="iris_z", target_bone="j_f_irisprm_l", transform_type="SCALE_Y", transform_space="LOCAL_SPACE"),
+                           #Don't mind that it's taking the Y scale, it works
+                       ]
+                   ),
+                   time="Post",
+               )
             ]
         ),
         ExtensionBone(
