9be8dbe7e25a9d75043594042390867f8991712e
Oats <ayamichi.rei@gmail.com>
2026-06-02 19:14:31 +0200

    Face
    Part of spine module now

--- features/rigging/templates/modules/face/detailed.py
+++ features/rigging/templates/modules/face/detailed.py
@@ -11,8 +11,6 @@ from ......core import rigify
 HEAD = BoneGroup(
     name="Head",
     transform_link= [
-        TransformLink(target="DEF-Neck", bone="j_kubi"),
-        TransformLink(target="DEF-Head", bone="j_kao"),
         TransformLink(target="DEF-Cheek.B.R", bone="j_f_shoho_r"),
         TransformLink(target="DEF-Cheek.B.R.001", bone="j_f_dhoho_r"),
         TransformLink(target="DEF-Cheek.T.R", bone="j_f_hoho_r"),
@@ -28,27 +26,6 @@ HEAD = BoneGroup(
         TransformLink(target="DEF-Nostril.L", bone="j_f_hana_l"),
     ],
     generators=[
-        ConnectBone(
-            name="Neck",
-            bone_a="j_kubi",
-            bone_b="j_kao",
-            req_bones=["j_kubi", "j_kao"],
-            operations=[ParentBoneOperation(time="Pre", bone_name="Neck", parent=["Spine.004", "j_sebo_c"], is_connected=False),
-                        RigifyTypeOperation(time="Pre", bone_name="Neck", rigify_type=rigify.types.spines_super_head()),
-                        CollectionOperation(time="Pre", bone_name="Neck", collection_name="Head"),
-            ]
-        ),
-        ExtensionBone(
-            name="Head",
-            bone_a="j_kao",
-            axis_type="armature",
-            axis="Z",
-            size_factor=20.0,
-            parent="Neck",
-            is_connected=True,
-            req_bones=["j_kao"],
-            operations=[CollectionOperation(time="Pre", bone_name="Head", collection_name="Head")]
-        ),
         #Cheek Right
         ConnectBone(
             name="Cheek.B.R",
@@ -966,7 +943,6 @@ def get_rig_module() -> RigModule:
         ui_collections = UI_Collections([
             BoneCollection(name="Face (Primary)", ui=True, color_set="Face_Primary", row_index=0, title="Face (Primary)", visible=False),
             BoneCollection(name="Face (Secondary)", ui=True, color_set="Face_Secondary", row_index=0, title="Face (Secondary)", visible=False),
-            BoneCollection(name="Head", ui=True, color_set="Head", row_index=1, title="Head"),
         ]),
         operations=[
             #Left Face
